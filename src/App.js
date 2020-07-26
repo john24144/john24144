cd61ce04dad6a742c323136ebed720d9200f1f24
john24144 <john24144@gmail.com>
2020-07-25 20:48:23 -0400

    Added home and about sections, some styling, colors

--- src/App.js
+++ src/App.js
@@ -1,68 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+import circle from './imgs/circle.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
-      </header>
-    </div>
+    <Container fluid className="App">
+      <Home />
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+    </Container>  
   );
 }
 
 function Header() {
-  return(
-    <div className="Header">
-      <header className="Header-header">
-        there
-      </header>
-    </div>
+  return (
+    <Row className="bg-dark">
+      <Col xs={12} md={8} lg={6}>
+        <Nav className="rounded bg-light">
+          <Nav.Link className="text-secondary w-25">HOME</Nav.Link>
+          <Nav.Link className="text-secondary w-25">ABOUT</Nav.Link>
+          <Nav.Link className="text-secondary w-25">PROJECTS</Nav.Link>
+          <Nav.Link className="text-secondary w-25">CONTACT</Nav.Link>
+        </Nav>
+      </Col>
+    </Row>
   )
 }
 
 function Home() {
   return (
-    <div className="Home">
-      <header className="Home-header">
-        Obi
-      </header>
-    </div>
+    <Row className="Home justify-content-center">
+      <img src={circle} alt="" width="100%" height="100%" />
+      <Col className="text-center my-auto">
+        <h1>Keith Johnson</h1>
+        <h2>Software Developer</h2>
+      </Col>
+    </Row>
   );
 }
 
 function About() {
   return (
-    <div className="About">
-      <header className="About-header">
-        Wan
-      </header>
-    </div>
+    <Row className="About">
+      <Col className="text-center my-auto">
+        <h2>Hello.</h2>
+        <h5>
+          My name is Keith, and I'm about to finish my last year as a student at The Ohio State University.
+          So, now I'm looking for an entry level position that will test my abilities as a software developer.
+          Here's what you can count on me for:
+        </h5>
+        <h5>I have plenty of programming languages under my belt, plus I can learn fast.</h5>
+        <h5>I have previous experience interning as a web developer.</h5>
+        <h5>I know how to be a team player and/or team leader.</h5>
+        <h5>I'm excited to enter the field to grow even more as a software engineer.</h5>
+      </Col>
+    </Row>
   );
 }
 
 function Projects() {
   return (
-    <div className="Projects">
-      <header className="Projects-header">
-        
-      </header>
-    </div>
+    <Row className="Projects">
+      <Col className="text-center my-auto">
+        <h2>Projects</h2>
+      </Col>
+    </Row>
   );
 }
 
 function Contact() {
   return (
-    <div className="Contact">
-      <header className="Contact-header">
-        
-      </header>
-    </div>
+    <Row className="Contact bg-dark">
+      <Col>
+        <h2>Contact:</h2>
+        <p>email: john24144@gmail.com</p>
+      </Col>
+    </Row>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './fonts/Poly-Regular.otf';
+import './fonts/Poly-Italic.otf';
 
 ReactDOM.render(
   <React.StrictMode>
