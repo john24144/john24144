b151b8394fc2f1d2a2b9af85dbe93d7fd705d42d
john24144 <john24144@gmail.com>
2020-07-28 15:21:01 -0400

    Added contact info, more projects, navbar functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import circle from './imgs/circle.png';
 import headshot from './imgs/headshot.jpg';
 import CFG from './papers/CFG.pdf';
 import CFG_capture from './imgs/CFG_capture.png';
+import OSU_COVID_Hub_capture from './imgs/OSU_COVID_Hub_capture.PNG';
+import MealPlanner_capture from './imgs/MealPlanner_capture.PNG';
+import cascade_sim_capture from './imgs/cascade_sim_capture.png';
+//import streamer_capture from './imgs/streamer_capture.png';
+import linkedin from './imgs/linkedin.png';
+import github from './imgs/github.png';
+import email from './imgs/email.png';
 
 import './App.css';
 
@@ -14,7 +21,7 @@ function App() {
       <Header />
       <Home />
       <About />
-      <Projects />
+      <Portfolio />
       <Contact />
     </Container>  
   );
@@ -26,10 +33,10 @@ function Header() {
       <Col xs={12} md={8} lg={6}>
         <Navbar className="rounded bg-light">
           <Nav className="w-100">
-            <Nav.Link className="text-secondary w-25">HOME</Nav.Link>
-            <Nav.Link className="text-secondary w-25">ABOUT</Nav.Link>
-            <Nav.Link className="text-secondary w-25">PROJECTS</Nav.Link>
-            <Nav.Link className="text-secondary w-25">CONTACT</Nav.Link>
+            <Nav.Link href="#Start" className="text-secondary w-25">HOME</Nav.Link>
+            <Nav.Link href="#About" className="text-secondary w-25">ABOUT</Nav.Link>
+            <Nav.Link href="#Portfolio" className="text-secondary w-25">PORTFOLIO</Nav.Link>
+            <Nav.Link href="#Contact" className="text-secondary w-25">CONTACT</Nav.Link>
           </Nav>
         </Navbar>
       </Col>
@@ -39,22 +46,22 @@ function Header() {
 
 function Home() {
   return (
-    <>
-    <Row className="Spacer"></Row>
-    <Row className="Home justify-content-center">
+    <div id="Start">
+      <Row className="Spacer"></Row>
+      <Row className="Home justify-content-center">
       <img src={circle} alt="" width="100%" height="100%" />
       <Col className="text-center my-auto">
         <h1>Keith Johnson</h1>
         <h2>Software Developer</h2>
       </Col>
     </Row>
-    </>
+    </div>
   );
 }
 
 function About() {
   return (
-    <Row className="About">
+    <Row id="About">
       <Col className="text-center my-auto">
         <img src={headshot} alt="" />
         <h2>Hello.</h2>
@@ -72,29 +79,55 @@ function About() {
   );
 }
 
-function Projects() {
+function Portfolio() {
   return (
-    <Row className="Projects">
-      <Col  xs={12} md={6} lg={4} className="text-center my-auto">
-        <h5>Capstone</h5>
-      </Col>
-      <Col xs={12} md={6} lg={4} className="text-center my-auto">
-        <h5>Lyfe App</h5>
-      </Col>
-      <Col xs={12} md={6} lg={4} className="text-center my-auto">
-        <h5><a href={CFG}>Exploring Graph-based Methods in Malware Detection</a></h5>
-        <a href={CFG}><img src={CFG_capture} alt="" /></a>
+    <>
+    <Row id="Portfolio">
+      <Col className="text-center my-auto">
+        <h2>Projects:</h2>
       </Col>
     </Row>
+    <Row id="Projects">
+      <Col  xs={12} md={6} lg={4} className="text-center my-auto">
+        <h4>Meal Planner</h4>
+        <img src={MealPlanner_capture} alt="" />
+      </Col>
+      <Col xs={12} md={6} lg={4} className="text-center my-auto">
+        <h4>Streamer Hub Website</h4>
+        <a href="https://john24144.github.io/Streamer-hub-website/">link</a>
+      </Col>
+    </Row>
+    <Row>
+      <Col className="text-center my-auto">
+        <h2>Papers:</h2>
+      </Col>
+    </Row>
+    <Row id="Papers">
+      <Col xs={12} md={6} lg={4} className="text-center my-auto">
+        <h4>Exploring Graph-based Methods in Malware Detection</h4>
+        <a href={CFG}><img src={CFG_capture} alt="" /></a>
+      </Col>
+      <Col xs={12} md={6} lg={4} className="text-center my-auto">
+        <h4>OSU COVID Info Hub</h4>
+        <img src={OSU_COVID_Hub_capture} alt="" />
+      </Col>
+      <Col xs={12} md={6} lg={4} className="text-center my-auto">
+        <h4>Cascade Sim</h4>
+        <a href="https://john24144.github.io/cascade_sim/"><img src={cascade_sim_capture} alt="" /></a>
+      </Col>
+    </Row>
+    </>
   );
 }
 
 function Contact() {
   return (
-    <Row className="Contact bg-dark">
+    <Row id="Contact" className="bg-dark">
       <Col className="text-center my-auto">
-        <h2>Contact:</h2>
-        <p>email: john24144@gmail.com</p>
+        <h2>Contact Me:</h2>
+        <a href="https://www.linkedin.com/in/keith-johnson-397270140/"><img src={linkedin} alt=""></img></a>
+        <a href="https://github.com/john24144"><img src={github} alt=""></img></a>
+        <a href="mailto: john24144@gmail.com"><img src={email} alt=""></img></a>
       </Col>
     </Row>
   );
